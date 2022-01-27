e5d56e34c326ec995ec56842aea6e45cb43b99c3
jianghangqing <2233543613@qq.com>
2022-01-27 14:12:28 +0800

    add: 令牌桶算法的基本使用

--- src/main/java/com/sec/kill/resources/SecKillController.java
+++ src/main/java/com/sec/kill/resources/SecKillController.java
@@ -1,16 +1,24 @@
 package com.sec.kill.resources;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.sec.kill.service.StockService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/sec")
 public class SecKillController {
-    
+
     StockService stockService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecKillController.class);
 
     @Autowired
     public SecKillController(StockService stockService) {
@@ -19,6 +27,7 @@ public class SecKillController {
 
     /**
      * 可以解决问题（单机下） 缺点：一个线程拿到锁其他线程处于阻塞状态，用户体验差，服务器压力大，吞吐量小
+     *
      * @param id item
      * @return whether success
      */
@@ -37,7 +46,7 @@ public class SecKillController {
     }
 
 
-    @RequestMapping("/kill_optim")
+    @RequestMapping("/kill/optimistic")
     public String killOptimistic(Integer id) {
         try {
             synchronized (this) {   // 控制层的调用处加锁
@@ -49,5 +58,21 @@ public class SecKillController {
             return e.getMessage();
         }
 
+    }
+
+
+    // 令牌桶实例
+    private RateLimiter rateLimiter = RateLimiter.create(10);
+
+    @GetMapping("/kill/rate/limit")
+    public String limit(@RequestParam(required = true) Integer id) {
+        // 没有获取到令牌会阻塞等待直到获取
+//        LOGGER.info("等待的时间： " + rateLimiter.acquire());
+        if (!rateLimiter.tryAcquire(2, TimeUnit.SECONDS)) {
+            System.out.println("限定时间2s内没有获取到令牌token");
+            return "抢购失败";
+        }
+        System.out.println("处理业务....");
+        return "测试令牌桶";
     }
 }
